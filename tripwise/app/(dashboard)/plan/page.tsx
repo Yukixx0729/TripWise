8e9129a0477de83a4f030041d8408e981f4b70a4
曾玉彬 <yuki@192-168-1-106.tpgi.com.au>
2023-09-03 23:03:43 +1000

    basic setup for homepage

---
+++ tripwise/app/(dashboard)/plan/page.tsx
@@ -0,0 +1,5 @@
+const PlanPage = async () => {
+  return <div>My planned trips</div>;
+};
+
+export default PlanPage;
---
+++ tripwise/app/page.tsx
@@ -0,0 +1,27 @@
+import Link from "next/link";
+import { auth } from "@clerk/nextjs";
+
+export default async function Home() {
+  const { userId } = await auth();
+  let href = userId ? "/plan" : "/new-user";
+  return (
+    <div className="w-screen h-screen flex justify-center items-center">
+      <div className="w-full max-w-[600px] mx-auto">
+        <h1 className="text-6xl mb-4">
+          Your Ultimate Trip Planning Companion.
+        </h1>
+        <p className="text-2xl text-black/60 mb-4">
+          Discover the perfect way to plan your next adventure with ease. Our
+          app simplifies trip planning like never before.
+        </p>
+        <div>
+          <Link href={href}>
+            <button className="bg-blue-600 px-4 py-2 rounded-lg text-xl text-white">
+              Get started
+            </button>
+          </Link>
+        </div>
+      </div>
+    </div>
+  );
+}
