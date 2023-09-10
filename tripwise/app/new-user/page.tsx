a3977df92134558d45df0e2c5c8b1124981ca6fc
曾玉彬 <yuki@hitamaakirasair.lan>
2023-09-10 14:33:43 +1000

    fixed new page error

--- tripwise/app/new-user/page.tsx
+++ tripwise/app/new-user/page.tsx
@@ -4,19 +4,20 @@ import { redirect } from "next/navigation";
 
 const createNewUser = async () => {
   const user = await currentUser();
-  const match = await prisma.user.findUnique({
-    where: {
-      clerkId: user.id as string,
-    },
-  });
-
-  if (!match) {
-    await prisma.user.create({
-      data: {
-        clerkId: user.id,
-        email: user?.emailAddresses[0].emailAddress as string,
+  if (user) {
+    const match = await prisma.user.findUnique({
+      where: {
+        clerkId: user.id as string,
       },
     });
+    if (!match) {
+      await prisma.user.create({
+        data: {
+          clerkId: user.id,
+          email: user?.emailAddresses[0].emailAddress as string,
+        },
+      });
+    }
   }
 
   redirect("/plan");
